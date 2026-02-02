799639ac8c1bb9f33943f932becf2dbc3c1b7511
Robert S Sheff <bsheff2@gmail.com>
2026-02-01 22:14:17 -0500

    Update WiFi credentials and GMT offset

--- examples/LVGL_Factory/LVGL_Factory.ino
+++ examples/LVGL_Factory/LVGL_Factory.ino
@@ -12,12 +12,12 @@
 #define SCREEN_WIDTH         128
 #define SCREEN_WIDTH         128
 
-#define WIFI_SSID            "Your-ssid"
-#define WIFI_PASSWORLD       "Your-password"
+#define WIFI_SSID            "dorobob"
+#define WIFI_PASSWORLD       "2palmbeach32"
 
 #define NTP_SERVER1          "pool.ntp.org"
 #define NTP_SERVER2          "time.nist.gov"
-#define GMT_OFFSET_SEC       (3600 * 8)
+#define GMT_OFFSET_SEC       (3600 * -4)
 #define DAY_LIGHT_OFFSET_SEC 0
 
 
